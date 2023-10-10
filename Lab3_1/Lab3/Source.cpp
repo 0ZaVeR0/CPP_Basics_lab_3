73a743fc72bb5a48ca7c8c71e911209b41952ece
0ZaVeR0 <69377579+0ZaVeR0@users.noreply.github.com>
2023-10-10 23:00:58 +0300

    Add files via upload

---
+++ Lab3_1/Lab3/Source.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+#define N1 5
+#define N2 5
+		
+void initmatr(int M[],int s)
+{
+    for (int i = 0; i < s; i++)
+        M[i] = rand() % 100 / 10.;
+}
+
+int rowmax(int M[],int n)
+{
+    int max = -200;
+    int m_index = 0;
+    for (int i = 0; i < n; i++) {
+        if (max < M[i]) {
+            max = M[i];
+            m_index = i;
+        }
+    }
+    return m_index;
+}
+
+int sum_of_2nd(int M[],int n, int max) {
+    int sum = 0;
+    for (int i = max; i < n; i++) {
+        sum += M[i];
+    }
+    return sum;
+}
+
+void printmatr(int M[N1])
+{
+    int i, j;
+    for (i = 0; i < N1; i++)
+    {
+        cout << M[i] << ", ";
+    }
+    cout << "\n";
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int A[N1], B[N2];
+    int sum_A, sum_B;
+
+    initmatr(A,N1);
+    initmatr(B,N2);
+
+    printmatr(A);
+    printmatr(B);
+
+    sum_A = sum_of_2nd(A, N1, rowmax(A,N1));
+    sum_B = sum_of_2nd(B, N2, rowmax(B, N2));
+
+    if (sum_A < sum_B) {
+        cout << "Сумма элементов второй части меньше в массиве A";
+    }
+    else if (sum_A > sum_B) {
+        cout << "Сумма элементов второй части меньше в массиве B";
+    }
+    else {
+        cout << "Сумма элементов второй части массивов равна";
+    }
+
+    return 0;
+}
---
+++ Lab3_2/Lab3_2/source.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+#define row 10
+#define col 10
+
+void initmatr(int M[row][col], int r, int c)
+{
+    int i, j;
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            M[i][j] = rand() % 100 / 10.;
+}
+
+void printmatr(int M[row][col], int r, int c)
+{
+    int i, j;
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            cout << M[i][j] << ", ";
+        }
+        cout << "\n";
+    }
+}
+
+int sum(int M[row][col], int r, int c) {
+    int center = row / 2;
+    int sum = 0;
+
+    for (int i = 0; i < center; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            cout << M[i][j] << " , " << M[i][col- j - 1] << ", ";
+            sum = sum + M[i][j] + M[i][col - j - 1];
+        }
+        cout << "\n";
+    }
+    for (int i = center; i < row; i++) {
+        for (int j = 0; j < 10-i; j++) {
+            cout << M[i][j] << " , " << M[i][col - j - 1] << ", ";
+            sum = sum + M[i][j] + M[i][col - j - 1];
+        }
+        cout << "\n";
+    }
+    return sum;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int A[row][col];
+
+    initmatr(A,row,col);
+    printmatr(A, row, col);
+    cout << sum(A,row, col);
+
+    return 0;
+}
